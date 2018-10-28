bcafffd271f463c50f9829cb2605347fccad4173
Brian Yuhas <btyuhas@vt.edu>
2018-10-27 23:32:57 -0400

    Update mnist-cnn.py

--- mnist-cnn.py
+++ mnist-cnn.py
@@ -11,7 +11,7 @@ model = keras.models.Sequential()
 
 model.add(Conv2D(32, (3, 3), activation='relu', input_shape=[28, 28, 1]))
 model.add(Conv2D(64, (3, 3), activation='relu'))
-#model.add(MaxPooling2D((2, 2)))
+model.add(MaxPooling2D((2, 2)))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
