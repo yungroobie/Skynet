62863b4e4019cc51fc013afe1c5e461b77ec6852
btyuhas <btyuhas@vt.edu>
2018-10-27 22:51:53 -0400

    simplified

--- mnist-cnn.py
+++ mnist-cnn.py
@@ -1,56 +1,28 @@
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten
-from tensorflow.keras.layers import Conv2D, MaxPooling2D
-from tensorflow.keras.utils import normalize, to_categorical
-
-# Load MNIST dataset
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
-
-# Reshape and preprocess images
-X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
-X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
-
-X_train = normalize(X_train)
-X_test = normalize(X_test)
-
-y_train = to_categorical(y_train, 10)
-y_test = to_categorical(y_test, 10)
-
-# Define the model
-model = Sequential()
+import keras
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+# Load MNIST dataset -----------------------------------------------------------
+(X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
+# Preprocessing ----------------------------------------------------------------
+X_train = X_train.reshape(-1, 28, 28, 1).astype('float16') / 255.0
+X_test = X_test.reshape(-1, 28, 28, 1).astype('float16') / 255.0
+# Define the model -------------------------------------------------------------
+model = keras.models.Sequential()
 
 model.add(Conv2D(32, (3, 3), activation='relu', input_shape=[28, 28, 1]))
-model.add(Conv2D(32, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
 model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
+model.add(MaxPooling2D((2, 2)))
 model.add(Flatten())
-
 model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(10, activation='softmax'))
 
-# Print the architecture of the model
 print(model.summary())
-
-# Define loss functions, optimizer, and metrics to track while training
-model.compile(loss='categorical_crossentropy',
+# Compile model ----------------------------------------------------------------
+model.compile(loss='sparse_categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
-
-# Fit the model to the training dataset
-model.fit(X_train, y_train,
-          batch_size=64,
-          epochs=10,
-          validation_split=0.2)
-
-# Evaluate model performance
-score = model.evaluate(X_test, y_test, verbose=0)
-print('Test loss:', score[0])
+# Fit the model to the training set --------------------------------------------
+model.fit(X_train, y_train, batch_size=32, epochs=10, validation_split=0.2)
+# Evaluate model performance ---------------------------------------------------
+score = model.evaluate(X_test, y_test)
 print('Test accuracy:', score[1])
