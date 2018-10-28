aea81acfcee5a2cf3c70be49186b34ea03d6a85c
btyuhas <btyuhas@vt.edu>
2018-10-27 23:28:24 -0400

    converted to .py

--- mnist-cnn.py
+++ mnist-cnn.py
@@ -1,16 +1,17 @@
 import keras
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+
 # Load MNIST dataset -----------------------------------------------------------
 (X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
 # Preprocessing ----------------------------------------------------------------
-X_train = X_train.reshape(-1, 28, 28, 1).astype('float16') / 255.0
-X_test = X_test.reshape(-1, 28, 28, 1).astype('float16') / 255.0
+X_train = X_train.reshape(-1, 28, 28, 1).astype('float32') / 255
+X_test = X_test.reshape(-1, 28, 28, 1).astype('float32') / 255
 # Define the model -------------------------------------------------------------
 model = keras.models.Sequential()
 
 model.add(Conv2D(32, (3, 3), activation='relu', input_shape=[28, 28, 1]))
 model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D((2, 2)))
+#model.add(MaxPooling2D((2, 2)))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
@@ -19,10 +20,11 @@ model.add(Dense(10, activation='softmax'))
 print(model.summary())
 # Compile model ----------------------------------------------------------------
 model.compile(loss='sparse_categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+              optimizer='adam', metrics=['accuracy'])
 # Fit the model to the training set --------------------------------------------
 model.fit(X_train, y_train, batch_size=32, epochs=10, validation_split=0.2)
 # Evaluate model performance ---------------------------------------------------
 score = model.evaluate(X_test, y_test)
 print('Test accuracy:', score[1])
+# Save model -------------------------------------------------------------------
+model.save('mnist.h5')
---
+++ preprocessing.py
@@ -0,0 +1,31 @@
+import os
+import zipfile
+import fnmatch
+import shutil
+
+# File paths -------------------------------------------------------------------
+# path to directory of zipped files from USGS
+source = "C:\\Users\\Brian\\Desktop\\USGS"
+# path to new directory containing images to label
+dest = "C:\\Users\\Brian\\Desktop\\images"
+# Unzip USGS files ------------------------------------------------------------
+os.chdir(source)
+for file in os.listdir():
+    with zipfile.ZipFile(file, 'r') as zip_ref:
+        zip_ref.extractall()
+# Recursively search for images and save paths --------------------------------
+images = []
+for root, dirs, files in os.walk(source+"\\VA"):
+    for name in files:
+        if fnmatch.fnmatch(name, '*.tif'):
+            images.append(os.path.join(root, name))
+# Move images into new directory ----------------------------------------------
+os.mkdir(dest)
+for image in images:
+    shutil.move(image, dest)
+# Rename files to make relationships clear ------------------------------------
+os.chdir(dest)
+files = os.listdir()
+for i in range(0, len(files), 2):
+    os.rename(files[i], f"{i}_new.tif")
+    os.rename(files[i+1], f"{i}_old.tif")
