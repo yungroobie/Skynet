8e4b11b4aa2646834b0ff4d2a0a4d6b1ce4fc6d2
btyuhas <btyuhas@vt.edu>
2018-11-29 15:56:46 -0500

    draw boxes directly

--- YOLOv3/canvassing_updated.py
+++ YOLOv3/canvassing_updated.py
@@ -2,8 +2,9 @@ import matplotlib.pyplot as plt
 from scipy.misc import imsave
 import os
 import numpy as np
-
-
+import pickle
+from utils.utils import do_nms
+from utils.bbox import draw_boxes
 
 img = plt.imread("7_old.tif")[:, :, [0,1,2]]
 t = 5
@@ -22,19 +23,28 @@ for i in range(t):
 		endy = starty + tileSpace + extraTileSpace
 		temp = img[int(round(startx)):int(round(endx)), int(round(starty)):int(round(endy))]
 		imsave(f"tiles/{i}_{j}.jpg", temp)
-os.system("python predict.py -c config.json -i tiles/")
-os.system("mv output/*.jpg tiles/")
-out = np.zeros((5000, 5000, 3), dtype=np.uint16)
-os.chdir("tiles")
-for image in os.listdir():
-	temp = plt.imread(image)
-	row, col = map(int, image.split('.')[0].split('_'))
-	startx = ((row*tileSpace) - row * (2*e)) + (row*extraTileSpace)
-	endx = startx + tileSpace + extraTileSpace
-	starty = ((col*tileSpace) - col * (2*e)) + (col*extraTileSpace)
-	endy = starty + tileSpace + extraTileSpace
-	out[int(round(startx)):int(round(endx)), int(round(starty)):int(round(endy))] = temp
 
-imsave('../output.png', out)
+os.system("python predict.py -c config.json -i tiles/")
+
+os.chdir("tiles")
 os.system("rm *")
 os.system("mv ../output/*.pkl .")
+
+boxes = []
+for tile in os.listdir():
+	row, col = map(int, tile.split('.')[0].split('_'))
+	tile_boxes = pickle.load(open(tile, 'rb'))
+	# adjust boxes
+	for box in tile_boxes:
+		box.xmin = box.xmin + finalTileSize * row
+		box.xmax = box.xmax + finalTileSize * row
+		box.ymin = box.ymin + finalTileSize * col
+		box.ymax = box.ymax + finalTileSize * col
+
+		boxes.append()
+
+do_nms(boxes, .35)
+out = draw_boxes(img, boxes, ['other', 'residential'], 0.35)
+
+
+imsave('../output.png', out)
